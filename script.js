cce1aa9b4c7094c6e921ae84a6955cae618609ad
Eviive <albert.vaillon21@gmail.com>
2022-01-16 17:12:25 +0100

    Added a play button / function

--- script.js
+++ script.js
@@ -1,97 +1,105 @@
 /**
- * @returns fills the grid with rows * columns squares
+ * @returns Fills the grid with rows * columns squares
  */
 function SetupGrid() {
-	const container = $('.container')
-	var square = document.createElement('div')
-	square.className = 'square'
-	container.attr('style', `--number: ${nb_rows};`)
+	let container = $('.container');
+	let square = document.createElement('span');
+	square.className = 'square';
+	container.attr('style', `--sq-number: ${nb_rows}; --sq-size: ${sq_size}px`);
 	for (let i = 0; i < nb_rows * nb_columns; i++) {
-		container.append(square.cloneNode(true))
+		container.append(square.cloneNode(true));
 	}
 }
 
 /**
- * @returns creates and fills the 2D array of squares
+ * @returns {Array.<Array.<HTMLSpanElement>>} Creates and fills the 2D array of squares
  */
 function SetupArray() {
-	let square_array = []
-	let squares = $('.square')
+	let square_array = [];
+	let squares = $('.square');
 
-	let i = 0
+	let i = 0;
 	while (i < nb_rows) {
-		square_array[i] = []
-		i++
+		square_array[i] = [];
+		i++;
 	}
 
-	i = 0
-	let j
+	i = 0;
+	let j;
 	while (i < nb_rows) {
-		j = 0
+		j = 0;
 		while (j < nb_columns) {
-			square_array[i][j] = squares[j + nb_rows * i]
-			j++
+			square_array[i][j] = squares[j + nb_rows * i];
+			j++;
 		}
-		i++
+		i++;
 	}
 
-	return square_array
+	return square_array;
 }
 
 /**
- * @returns adds the click event that adds the 'spawn' class
+ * @returns Adds the click event that adds the 'spawn' class
  */
 function Spawn() {
-	$('html').off('click')
-	$('html').off('mouseover')
-	if (!cpt_spawn) {
+	$('html').off('click');
+	$('html').off('mouseover');
+	if (cpt_spawn) {
 		$('html').click( e => {
 			if (e.target.matches('.square') && !e.target.matches('.destination')) {
-				e.target.classList.add('spawn')
-				e.target.classList.remove('wall')
-				cpt_spawn = true
-				$('html').off('click')
+				e.target.classList.add('spawn');
+				e.target.classList.remove('wall');
+				cpt_spawn = false;
+				$('.btn-spawn').text('Place spawn : 0');
+				$('html').off('click');
 			}
 		})
 	}
 }
 
 /**
- * @returns adds the click event that adds the 'destination' class
+ * @returns Adds the click event that adds the 'destination' class
  */
  function Destination() {
-	$('html').off('click')
-	$('html').off('mouseover')
-	if (!cpt_destination) {
+	$('html').off('click');
+	$('html').off('mouseover');
+	if (cpt_destination) {
 		$('html').click( e => {
 			if (e.target.matches('.square') && !e.target.matches('.spawn')) {
-				e.target.classList.add('destination')
-				e.target.classList.remove('wall')
-				cpt_destination = true
-				$('html').off('click')
+				e.target.classList.add('destination');
+				e.target.classList.remove('wall');
+				cpt_destination = false;
+				$('.btn-destination').text('Place destination : 0');
+				$('html').off('click');
 			}
 		})
 	}
 }
 
 /**
- * @returns adds a click event that that triggers the mouseover event that adds the 'wall' class
+ * @returns Adds a click event that that triggers the mouseover event that adds the 'wall' class
  */
 function Walls() {
-	$('html').off('click')
-	$('html').off('mouseover')
+	$('html').off('click');
+	$('html').off('mouseover');
 	$('html').click( e => {
 		if (e.target.matches('.square')) {
-			e.target.classList.add('wall')
+			e.target.classList.add('wall');
 			$('html').mouseover( f => {
 				if (f.target.matches('.square')) {
-					f.target.classList.add('wall')
+					f.target.classList.add('wall');
 				}
 			})
 			$('html').click( g => {
 				if (g.target.matches('.square') || g.target.matches('.btn-wall') || g.target.matches('.btn-spawn')) {
-					$('html').off('click')
-					$('html').off('mouseover')
+					$('html').off('click');
+					$('html').off('mouseover');
+				}
+			})
+			$('html').mouseover( g => {
+				if (!$(g.target).closest('.container').length) {
+					$('html').off('click');
+					$('html').off('mouseover');
 				}
 			})
 		}
@@ -99,57 +107,127 @@ function Walls() {
 }
 
 /**
- * @returns resets the grid
+ * @returns Resets the grid
  */
 function Reset() {
-	let reset = $('.btn-reset')
-	let reset_img = $('.btn-reset img')
-	reset.off('click')
-	reset_img.addClass('active')
+	let reset = $('.btn-reset');
+	let reset_img = $('.btn-reset img');
+	reset.off('click');
+	reset_img.addClass('active');
 	setTimeout( _ => {
-		reset_img.removeClass('active')
-		reset.click( _ => Reset() )
-	}, 2000)
-	$('.spawn, .destination, .wall').removeClass(['spawn', 'destination', 'wall'])
-	cpt_spawn = false
-	cpt_destination = false
+		reset_img.removeClass('active');
+		reset.click( _ => Reset() );
+	}, 1500)
+	$('.spawn, .destination, .wall').removeClass(['spawn', 'destination', 'wall']);
+	cpt_spawn = true;
+	$('.btn-spawn').text('Place spawn : 1');
+	cpt_destination = true;
+	$('.btn-destination').text('Place destination : 1');
 }
 
 /**
- * @param {HTMLElement} square
- * @returns {[number, number]} the coordinates of the square
+ * @param {HTMLSpanElement} square
+ * @returns {[number, number]} The coordinates of the square
  */
 function GetCoordinates(square) {
-	let ct = document.querySelector('.container')
+	let ct = $('.container');
 
-	let ct_left = ct.offsetLeft
-	let ct_top = ct.offsetTop
+	let ct_left = ct.offset().left;
+	let ct_top = ct.offset().top;
 
-	let sq_column = Math.abs((ct_left - square.offsetLeft) / 17)
-	let sq_row = Math.abs((ct_top - square.offsetTop) / 17)
+	let sq_row = Math.abs((ct_top - square.offset().top) / sq_size);
+	let sq_column = Math.abs((ct_left - square.offset().left) / sq_size);
 
-	return [sq_column, sq_row]
+	return [sq_row, sq_column];
+	/***************get the i and j directly from the array, not the offset***************/
 }
 
 /**
- * @returns adds the 'parcours' class to the diagonal and anti-diagonal
+ * @returns Adds the 'parcours' class to the desired paths
  */
-function Diagonal() {
-	let i = 0, j = 0
-
-	while (i < nb_columns && j < nb_rows) {
-		$(square_array[i][j]).addClass('parcours')
-		i++
-		j++
+function ParcoursTest() {
+	/***************diagonal***************/
+	let i = 0, j = 0;
+	while (i < nb_rows && j < nb_columns) {
+		$(square_array[i][j]).addClass('parcours');
+		i++;
+		j++;
 	}
 
-	i = nb_columns - 1
-	j = 0
+	/***************anti-diagonal***************/
+	i = nb_rows - 1;
+	j = 0;
+	while (i >= 0 && j < nb_columns) {
+		$(square_array[i][j]).addClass('parcours');
+		i--;
+		j++;
+	}
 
-	while (i >= 0 && j < nb_rows) {
-		$(square_array[i][j]).addClass('parcours')
-		i--
-		j++
+	/***************middle vertical***************/
+	i = 0;
+	j = Math.floor(nb_columns / 2);
+	while (i < nb_rows) {
+		$(square_array[i][j]).addClass('parcours');
+		i++;
+	}
+
+	/***************middle horizontal***************/
+	i = Math.floor(nb_rows / 2);
+	j = 0;
+	while (j < nb_columns) {
+		$(square_array[i][j]).addClass('parcours');
+		j++;
+	}
+
+	/***************vertical sides***************/
+	i = 0;
+	j = 0;
+	while (i < nb_rows) {
+		$(square_array[i][j]).addClass('parcours');
+		$(square_array[i][j + nb_columns - 1]).addClass('parcours');
+		i++;
+	}
+
+	/***************horizontal sides***************/
+	i = 0;
+	j = 0;
+	while (j < nb_columns) {
+		$(square_array[i][j]).addClass('parcours');
+		$(square_array[i + nb_rows - 1][j]).addClass('parcours');
+		j++;
+	}
+}
+
+/**
+ * @returns Sets up the click event on the play button
+ */
+function Event_Play() {
+	$('.btn-play').click( _ => {
+		var [spawn_row, spawn_column, destination_row, destination_column] = Play();
+		console.log(spawn_row, spawn_column, destination_row, destination_column);
+	})
+}
+
+/**
+ * @returns Returns the coordinates of the spawn and the destination if they exist
+ */
+function Play() {
+	let play = $('.btn-play');
+	play.off('click');
+
+	let sq_spawn = $('.spawn');
+	let sq_destination = $('.destination');
+
+	if (sq_spawn.length && sq_destination.length) {
+		let [spawn_row, spawn_column] = GetCoordinates(sq_spawn);
+		let [destination_row, destination_column] = GetCoordinates(sq_destination);
+		Event_Play();
+		return [spawn_row, spawn_column, destination_row, destination_column];
+	}
+	else
+	{
+		Event_Play();
+		return [];
 	}
 }
 
@@ -158,20 +236,23 @@ function Diagonal() {
 
 
 /***************number of columns and rows***************/
-var nb_rows, nb_columns
-nb_rows = nb_columns = 45
+var nb_rows, nb_columns, sq_size;
+nb_rows = nb_columns = 45;
+sq_size = 17;
 
-SetupGrid()
-square_array = SetupArray()
+SetupGrid();
+square_array = SetupArray();
 
-cpt_spawn = false
+cpt_spawn = true;
 $('.btn-spawn').click( _ => Spawn() )
 
-cpt_destination = false
+cpt_destination = true;
 $('.btn-destination').click( _ => Destination() )
 
 $('.btn-wall').click( _ => Walls() )
 
 $('.btn-reset').click( _ => Reset() )
 
-Diagonal()
+Event_Play();
+
+ParcoursTest();
