02f1124b1bacd57ed6adae5822345536b18aa45a
Eviive <albert.vaillon21@gmail.com>
2022-02-12 00:34:46 +0100

    Redesign of the UI

--- script.js
+++ script.js
@@ -1,39 +1,33 @@
-// TODO: templates if every block is missing (via JSON maybe)
-// TODO: do a larger cursor in order to place walls easily
+// TODO: plusieurs algorithmes
+// TODO: plusieurs vitesses d'algorithme
+// TODO: génération d'obstacles
 
 let body = document.body;
 let canvas = document.querySelector("canvas");
 let context = canvas.getContext("2d");
 
-let btnPopup = document.querySelector("#btn-popup");
-let btnSpawn = document.querySelector("#btn-spawn");
-let btnDestination = document.querySelector("#btn-destination");
-let btnWall = document.querySelector("#btn-wall");
 let btnPlay = document.querySelector("#btn-play");
 let btnReset = document.querySelector("#btn-reset");
 
-let btnStatus = null;
-let spawnStatus = false;
-let destinationStatus = false;
-let launchedStatus = false;
-
 const lineWidth = 2;
-const colorPath = "#ffffff66";
 const colorWhite = "#ffffff";
-const colorGreen = "#27d507";
+const colorGray = "#c9c9c9"
+const colorGreen = "#20ab07";
 const colorRed = "#ff0000";
-const colorBlue = "#0000e8";
+const colorLightBlue = "#0061ff";
+const colorDarkBlue = "#003080"
 
-const gridSize = 40;
-let squareSize;
-let canvasMap = createArray(gridSize, gridSize);
+let canvasMap;
+let squareSize = 30;
+let gridSizeWidth, gridSizeHeight;
 let spawnX, spawnY;
 let destinationX, destinationY;
 
-let distance = createArray(gridSize, gridSize);
-let visited = createArray(gridSize, gridSize);
-let predecessor = createArray(gridSize, gridSize);
+let distance;
+let visited;
+let predecessor;
 let foundPath;
+let launchedStatus = false;
 
 
 /**
@@ -56,32 +50,29 @@ function createArray(length) {
  * @param {Number} y Y position
  * @returns {Boolean} checks if the coordinates are part of the exterior walls
  */
-let exteriorWallsTest = (x, y) => {
-	if (x == 0 || x == canvas.width / squareSize - 1 || y == 0 || y == canvas.height / squareSize - 1) {
+let outsideGridTest = (x, y) => {
+	if (x < 0 || y < 0 || x >= gridSizeWidth || y >= gridSizeHeight) {
 		return true;
 	}
 	return false;
 };
 
 class Square {
-	static ratio = 1;
-
-	constructor(xPos, yPos, size, color) {
-		this.xPos = xPos + lineWidth / 1.2;
-		this.yPos = yPos + lineWidth / 1.2;
-		this.size = size - lineWidth * 1.2;
+	constructor(xPos, yPos, xSize, ySize, color) {
+		this.xPos = xPos;
+		this.yPos = yPos;
+		this.xSize = xSize;
+		this.ySize = ySize;
 		this.color = color;
 	}
 
 	draw() {
 		context.fillStyle = this.color;
-		context.fillRect(this.xPos * Square.ratio, this.yPos * Square.ratio, this.size * Square.ratio, this.size);
+		context.fillRect(this.xPos, this.yPos, this.xSize, this.ySize);
 	}
 }
 
 class Circle {
-	static ratio = 1;
-
 	constructor(xPos, yPos, radius, color) {
 		this.xPos = xPos + squareSize / 2;
 		this.yPos = yPos + squareSize / 2;
@@ -92,7 +83,7 @@ class Circle {
 	draw() {
 		context.beginPath();
 		context.lineWidth = lineWidth;
-		context.arc(this.xPos * Circle.ratio, this.yPos * Circle.ratio, this.radius * Circle.ratio, 0, Math.PI * 2, false);
+		context.arc(this.xPos, this.yPos, this.radius, 0, Math.PI * 2, false);
 		context.fillStyle = this.color;
 		context.fill();
 		context.closePath();
@@ -103,75 +94,48 @@ class Circle {
  * @returns adapts the size of the canvas
  */
 function canvasSize() {
-	canvas.width = Math.min(.85 * body.clientWidth, .7 * body.clientHeight);
-	canvas.height = canvas.width;
-	squareSize = canvas.width / gridSize;
-}
-
-/**
- * @returns draws the grid responsively
- */
-function drawGrid() {
-	let cSize = canvas.width;
-
-	context.beginPath();
-	context.lineWidth = lineWidth;
-	context.strokeStyle = colorPath;
-
-	for (let i = 0; i <= cSize; i = i + squareSize) {
-		/***************rows***************/
-		context.moveTo(0, i);
-		context.lineTo(cSize, i);
-		
-		/***************columns***************/
-		context.moveTo(i, 0);
-		context.lineTo(i, cSize);
-	}
-
-	context.stroke();
-	context.closePath();
+	let header = document.querySelector("#menu");
+	canvas.width = (body.clientWidth - header.clientWidth) - (body.clientWidth - header.clientWidth) % squareSize;
+	canvas.height = body.clientHeight - body.clientHeight % squareSize;
+	gridSizeWidth = canvas.width / squareSize;
+	gridSizeHeight = canvas.height / squareSize;
+	canvasMap = createArray(gridSizeWidth, gridSizeHeight);
+	distance = createArray(gridSizeWidth, gridSizeHeight);
+	visited = createArray(gridSizeWidth, gridSizeHeight);
+	predecessor = createArray(gridSizeWidth, gridSizeHeight);
+	resetMap();
 }
 
 /**
  * @returns draws the exterior walls
  */
-function exteriorWalls() {
-	for (let i = 0; i < canvas.width / squareSize; i++) {
-		for (let j = 0; j < canvas.width / squareSize; j++) {
-			if (exteriorWallsTest(i, j)) {
-				canvasMap[i][j] = "wall";
-			}
-			else {
-				canvasMap[i][j] = "empty";
-			}
+function resetMap() {
+	launchedStatus = false;
+	for (let i = 0; i < Math.round(canvas.width / squareSize); i++) {
+		for (let j = 0; j < Math.round(canvas.height / squareSize); j++) {
+			canvasMap[i][j] = "empty";
 		}
 	}
-	spawnStatus = false;
-	btnSpawn.innerHTML = "Place spawn : 1";
-	destinationStatus = false;
-	btnDestination.innerHTML = "Place destination : 1";
+	canvas.addEventListener("click", clickCanvas, {once: true});
+	randomGeneration();
 }
 
 /**
  * @returns generates a random spawn and destination
  */
 function randomGeneration() {
-	spawnX = Math.round(Math.random() * (((canvas.width - 2 * squareSize) / squareSize) - 1) + 1);
-	spawnY = Math.round(Math.random() * (((canvas.width - 2 * squareSize) / squareSize) - 1) + 1);
-	if (!spawnStatus) {
-		canvasMap[spawnX][spawnY] = "spawn";
-		spawnStatus = true;
-		btnSpawn.innerHTML = "Place spawn : 0";
+	if (canvas.width + 100 < canvas.height) {
+		spawnX = destinationX = Math.floor(canvas.width / squareSize / 2);
+		spawnY = Math.floor(canvas.height / squareSize * 4/5);
+		destinationY = Math.floor(canvas.height / squareSize * 1/5);
 	}
-	if (!destinationStatus) {
-		do {
-			destinationX = Math.round(Math.random() * (((canvas.width - 2 * squareSize) / squareSize) - 1) + 1);
-			destinationY = Math.round(Math.random() * (((canvas.width - 2 * squareSize) / squareSize) - 1) + 1);
-		} while (destinationX == spawnX && destinationY == spawnY);
-		canvasMap[destinationX][destinationY] = "destination";
-		destinationStatus = true;
-		btnDestination.innerHTML = "Place destination : 0";
+	else {
+		spawnY = destinationY = Math.ceil(canvas.height / squareSize / 2) - 1;
+		spawnX = Math.floor(canvas.width / squareSize * 1/5);
+		destinationX = Math.floor(canvas.width / squareSize * 4/5);
 	}
+	canvasMap[spawnX][spawnY] = "spawn";
+	canvasMap[destinationX][destinationY] = "destination";
 }
 
 /**
@@ -179,123 +143,83 @@ function randomGeneration() {
  */
  function renderCanvas() {
 	context.clearRect(0, 0, canvas.width, canvas.height);
-	drawGrid();
 	if (launchedStatus) {
 		dijkstra();
 	}
-	for (let i = 0; i < canvas.width / squareSize; i++) {
-		for (let j = 0; j < canvas.height / squareSize; j++) {
+	for (let i = 0; i < Math.round(canvas.width / squareSize); i++) {
+		for (let j = 0; j < Math.round(canvas.height / squareSize); j++) {
 			if (canvasMap[i][j] == "wall") {
-				let square = new Square(i * squareSize, j * squareSize, squareSize, colorWhite);
+				let square = new Square(i * squareSize, j * squareSize, squareSize, squareSize, colorDarkBlue);
 				square.draw();
 			}
 			else if (canvasMap[i][j] == "spawn") {
-				let square = new Square(i * squareSize, j * squareSize, squareSize, colorGreen);
+				let square = new Square(i * squareSize, j * squareSize, squareSize, squareSize, colorGreen);
 				square.draw();
 			}
 			else if (canvasMap[i][j] == "destination") {
-				let square = new Square(i * squareSize, j * squareSize, squareSize, colorRed);
+				let square = new Square(i * squareSize, j * squareSize, squareSize, squareSize, colorRed);
 				square.draw();
 			}
 			else if (canvasMap[i][j] == "parcours") {
-				let square = new Square(i * squareSize, j * squareSize, squareSize, colorBlue);
+				let square;
+				if ((i + j % 2) % 2 == 0) {
+					square = new Square(i * squareSize, j * squareSize, squareSize, squareSize, colorWhite);
+				}
+				else {
+					square = new Square(i * squareSize, j * squareSize, squareSize, squareSize, colorGray);
+				}
 				square.draw();
-				let circle = new Circle(i * squareSize, j * squareSize, squareSize / 7, colorRed);
+				let circle = new Circle(i * squareSize, j * squareSize, squareSize / 5, colorRed);
 				circle.draw();
 			}
 			else if(visited[i][j] && launchedStatus) {
-				let circle = new Circle(i * squareSize, j * squareSize, squareSize / 7, colorRed);
+				let square;
+				if ((i + j % 2) % 2 == 0) {
+					square = new Square(i * squareSize, j * squareSize, squareSize, squareSize, colorWhite);
+				}
+				else {
+					square = new Square(i * squareSize, j * squareSize, squareSize, squareSize, colorGray);
+				}
+				square.draw();
+				let circle = new Circle(i * squareSize, j * squareSize, squareSize / 7, colorLightBlue);
 				circle.draw();
+			}
+			else if (canvasMap[i][j] == "empty") {
+				let square;
+				if ((i + j % 2) % 2 == 0) {
+					square = new Square(i * squareSize, j * squareSize, squareSize, squareSize, colorWhite);
+				}
+				else {
+					square = new Square(i * squareSize, j * squareSize, squareSize, squareSize, colorGray);
+				}
+				square.draw();
 			}
 		}
 	}
 	requestAnimationFrame(renderCanvas);
+	// setTimeout(() => {
+	// 	renderCanvas();
+	// }, 1000/(60*5));
 }
 
 /***************Click handlers***************/
 
 /**
  * @param {EventListenerObject} event the object returned by the eventlistener that triggered the function
- * @returns changes the value of btnStatus according to which button triggered the event
- */
- function clickStatus(event) {
-	if (event.target == btnSpawn) {
-		btnStatus = "spawn";
-	}
-	else if (event.target == btnDestination) {
-		btnStatus = "destination";
-	}
-	else if (event.target == btnWall) {
-		btnStatus = "wall";
-	}
-}
-
-/**
- * @param {EventListenerObject} event the object returned by the eventlistener that triggered the function
  * @returns places blocks according to the value of btnStatus
  */
-function clickCanvas(event) {
-	if (btnStatus != null) {
-		if (btnStatus == "wall") {
-			canvas.addEventListener("mousemove", startMoveCanvas);
-			canvas.addEventListener("click", stopMoveCanvas, {once: true});
-			canvas.addEventListener("mouseout", stopMoveCanvas, {once: true});
-		}
-		else {
-			let canvasX = Math.floor((event.clientX - canvas.offsetLeft) / squareSize);
-			let canvasY = Math.floor((event.clientY - canvas.offsetTop) / squareSize);
-			if (!(exteriorWallsTest(canvasX, canvasY))) {
-				if (btnStatus == "spawn") {
-					if (canvasMap[canvasX][canvasY] == "spawn") {
-						canvasMap[canvasX][canvasY] = "empty";
-						btnSpawn.innerHTML = "Place spawn : 1";
-						spawnStatus = false;
-					}
-					else if (!spawnStatus) {
-						if (canvasMap[canvasX][canvasY] == "destination") {
-							btnDestination.innerHTML = "Place destination : 1";
-							destinationStatus = false;
-						}
-						canvasMap[canvasX][canvasY] = "spawn";
-						btnSpawn.innerHTML = "Place spawn : 0";
-						spawnStatus = true;
-						spawnX = canvasX;
-						spawnY = canvasY;
-					}
-					btnStatus = null;
-				}
-				else if (btnStatus == "destination") {
-					if (canvasMap[canvasX][canvasY] == "destination") {
-						canvasMap[canvasX][canvasY] = "empty";
-							btnDestination.innerHTML = "Place destination : 1";
-							destinationStatus = false;
-					}
-					else if (!destinationStatus) {
-						if (canvasMap[canvasX][canvasY] == "spawn") {
-							btnSpawn.innerHTML = "Place spawn : 1";
-							spawnStatus = false;
-						}
-						canvasMap[canvasX][canvasY] = "destination";
-						btnDestination.innerHTML = "Place destination : 0";
-						destinationStatus = true;
-						destinationX = canvasX;
-						destinationY = canvasY;
-					}
-					btnStatus = null;
-				}
-			}
-		}
+function clickCanvas() {
+	if (!launchedStatus) {
+		canvas.addEventListener("mousemove", startMoveCanvas);
+		canvas.addEventListener("click", stopMoveCanvas, {once: true});
+		canvas.addEventListener("mouseout", stopMoveCanvas, {once: true});
 	}
-	canvas.addEventListener("click", clickCanvas, {once: true});
 }
 
 /**
  * @returns starts the selected algorithm
  */
  function clickPlay() {
-	if (!spawnStatus && !destinationStatus) {
-		randomGeneration();	
-	}
 	dijkstraInit();
 	launchedStatus = true;
 	btnPlay.addEventListener("click", clickPlay, {once: true});
@@ -307,9 +231,7 @@ function clickCanvas(event) {
 function clickReset() {
 	let imgReset = btnReset.lastElementChild;
 	imgReset.classList.add("active");
-	launchedStatus = false;
-	canvas.addEventListener("click", clickCanvas, {once: true});
-	exteriorWalls();
+	resetMap();
 	setTimeout(() => {
 		imgReset.classList.remove("active");
 		btnReset.addEventListener("click", clickReset, {once: true});
@@ -325,10 +247,9 @@ function clickReset() {
  function startMoveCanvas(event) {
 	let canvasX = Math.floor((event.clientX - canvas.offsetLeft) / squareSize);
 	let canvasY = Math.floor((event.clientY - canvas.offsetTop) / squareSize);
-	if (canvasMap[canvasX][canvasY] != "spawn" && canvasMap[canvasX][canvasY] != "destination") {
+	if (!outsideGridTest(canvasX, canvasY) && canvasMap[canvasX][canvasY] != "spawn" && canvasMap[canvasX][canvasY] != "destination") {
 		canvasMap[canvasX][canvasY] = "wall";
 	}
-	btnStatus = null;
 }
 
 /**
@@ -336,7 +257,7 @@ function clickReset() {
  */
 function stopMoveCanvas() {
 	canvas.removeEventListener("mousemove", startMoveCanvas);
-	canvas.addEventListener("click", clickCanvas);
+	canvas.addEventListener("click", clickCanvas, {once: true});
 }
 
 /***************Dijkstra functions***************/
@@ -347,8 +268,8 @@ function stopMoveCanvas() {
  function dijkstraInit() {
 	canvas.removeEventListener("click", clickCanvas);
 	foundPath=false;
-	for (let i = 0; i < gridSize; i++) {
-		for (let j = 0; j < gridSize; j++) {
+	for (let i = 0; i < gridSizeWidth; i++) {
+		for (let j = 0; j < gridSizeHeight; j++) {
 			distance[i][j] = Number.MAX_VALUE;
 			visited[i][j] = false;
 			if (canvasMap[i][j] == "parcours") {
@@ -357,7 +278,7 @@ function stopMoveCanvas() {
 		}
 	}
 	distance[spawnX][spawnY] = 0;
-	predecessor = createArray(gridSize, gridSize);
+	predecessor = createArray(gridSizeWidth, gridSizeHeight);
 }
 
 /**
@@ -366,8 +287,8 @@ function stopMoveCanvas() {
 function dijkstraMinimum() {
 	let minDistance = Number.MAX_VALUE;
 	let minX = -1, minY = -1;
-	for (let i = 0; i < gridSize; i++) {
-		for (let j = 0; j < gridSize; j++) {
+	for (let i = 0; i < gridSizeWidth; i++) {
+		for (let j = 0; j < gridSizeHeight; j++) {
 			if (!visited[i][j] && canvasMap[i][j] != "wall" && distance[i][j] < minDistance) {
 				minDistance = distance[i][j];
 				minX = i;
@@ -386,7 +307,7 @@ function dijkstraMinimum() {
  * @returns the distance between point 1 and point 2
  */
 function dijkstraWeight(x1, y1, x2, y2) {
-	if (canvasMap[x2][y2] == "wall") {
+	if (outsideGridTest(x2, y2) || canvasMap[x2][y2] == "wall") {
 		return Number.MAX_VALUE;
 	}
 	if (x1 == x2 || y1 == y2) {
@@ -405,7 +326,7 @@ function dijkstraWeight(x1, y1, x2, y2) {
  */
 function dijsktraDistance(x1, y1, x2, y2) {
 	let majDistance = distance[x1][y1] + dijkstraWeight(x1,y1,x2,y2);
-	if (distance[x2][y2] > majDistance) {
+	if (!outsideGridTest(x2, y2) && distance[x2][y2] > majDistance) {
 		distance[x2][y2] = majDistance;
 		predecessor[x2][y2] = {x: x1, y: y1};
 	}
@@ -447,11 +368,6 @@ function dijkstra() {
 	}
 }
 
-
-btnSpawn.addEventListener("click", clickStatus);
-btnDestination.addEventListener("click", clickStatus);
-btnWall.addEventListener("click", clickStatus);
-
 btnPlay.addEventListener("click", clickPlay, {once: true});
 btnReset.addEventListener("click", clickReset, {once: true});
 
@@ -460,6 +376,6 @@ canvas.addEventListener("click", clickCanvas, {once: true});
 canvasSize();
 window.addEventListener("resize", canvasSize);
 
-exteriorWalls();
+resetMap();
 
 requestAnimationFrame(renderCanvas);
